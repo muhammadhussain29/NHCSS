2b33248bcc912f22a07dd2204ae1e4fdb855746f
muhammadhussain29 <hussainmughal29@gmail.com>
2024-03-10 23:58:37 +0500

    NHCSS image update testing-2

--- JS/event.js
+++ JS/event.js
@@ -31,6 +31,22 @@ let eventData = [
     time: '2:00 PM',
     location: 'Conference Hall',
     bgImg: "url('.imgs/poster.jpg')"
+  },
+  {
+    heading: 'Event 5',
+    speaker: 'Speaker 5',
+    date: '25-June-2023',
+    time: '2:00 PM',
+    location: 'Conference Hall',
+    bgImg: "url('.imgs/poster.jpg')"
+  },
+  {
+    heading: 'Event 6',
+    speaker: 'Speaker 6',
+    date: '25-June-2023',
+    time: '2:00 PM',
+    location: 'Conference Hall',
+    bgImg: "url('.imgs/poster.jpg')"
   }
 ];
 
